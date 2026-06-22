4f946bbc16438660c64b873c56bc7e2866bd45d1
Arushakanth <arushacse0210@gmail.com>
2026-06-22 21:04:27 +0530

    Update urls.py

--- crm_backend/urls.py
+++ crm_backend/urls.py
@@ -4,5 +4,5 @@ URL configuration for crm_backend project.
 from django.urls import path, include
 
 urlpatterns = [
-    path('https://backend-2v3h.onrender.com', include('enquiries.urls')),
+    path('api/', include('enquiries.urls')),
 ]
